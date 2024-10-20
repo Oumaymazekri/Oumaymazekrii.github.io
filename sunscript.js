0e640670dfdad79bf2547c8ffe12a839657f58f2
oumayma zekri <oum18157@gmail.com>
2024-10-20 11:03:10 +0100

    V2

--- sunscript.js
+++ sunscript.js
@@ -2,6 +2,5 @@ document.addEventListener('scroll', function() {
     const image = document.querySelector('.image-container img');
     let scrollPosition = window.scrollY;
     
-    // Appliquer un effet de décalage à l'image lors du défilement
     image.style.transform = 'translateY(' + scrollPosition * 0.2 + 'px)';
 });
